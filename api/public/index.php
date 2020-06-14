9fa6dd88a0430bcfa63e5aa51c8b2cb542577b1d
avi <abhi.uk96@gmail.com>
2020-06-15 00:05:02 +0100

    [docker] setup

---
+++ api/public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhi
+ * Date: 14/06/2020
+ * Time: 18:17
+ */
+
+echo "this is API page... install laravel";
+
---
+++ front/public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: abhi
+ * Date: 14/06/2020
+ * Time: 18:14
+ */
+
+echo "php -v ";
+
+echo "hello... this is front end page on port 8080  ";
