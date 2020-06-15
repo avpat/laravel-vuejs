5f2a14472532fb496343851298616d239cd0f859
avi <abhi.uk96@gmail.com>
2020-06-15 23:01:09 +0100

    [fix] typos

--- api/app/Http/Controllers/ChargeController.php
+++ api/app/Http/Controllers/ChargeController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Unit;
-use App\Charge;
 
 class ChargeController extends Controller
 {
@@ -94,7 +93,7 @@ class ChargeController extends Controller
             } else {
                 return response()->json([
                     'error' => [
-                        'message'       => 'An error occured. Id\'s wont match',
+                        'message'       => 'An error occured. Id\'s did not match',
                         'unitId'        => $unitId,
                         'chargeId'      => $chargeId,
                         'statusCode'    => 400
