135d33783992d506afa1f7d1f738f6d88f8fc5b0
avi <abhi.uk96@gmail.com>
2020-06-17 14:14:15 +0100

    [cleanup] code cleanup

---
+++ front/code/public/src/components/Units.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        <div v-bind:key="unit.id" v-for="unit in units">
+            <Unit v-bind:unit="unit" />
+        </div>
+    </div>
+</template>
+
+
+<script>
+import Unit from './Unit.vue'
+
+export default {
+  name: 'Units',
+  components: {
+      Unit
+  },
+  props: ["units"]
+}
+</script>
